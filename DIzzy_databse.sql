fbc7db95898a643f584a1640967333edb64a61f3
Kamila Nowak <knowak242@gmaik.com>
2019-11-25 14:54:03 +0100

    update

--- DIzzy_databse.sql
+++ DIzzy_databse.sql
@@ -9,7 +9,8 @@ CREATE TABLE `account`(
 `email` varchar(255) default NULL,
 `photo` varchar(256) DEFAULT NULL,
 `confirm_token` varchar(255) default null,
-
+`enabled` TINYINT NOT NULL,
+`mute` datetime DEFAULT NULL,
 primary key (`id`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
 
@@ -40,6 +41,13 @@ INSERT INTO `authorities` (authority) VALUES
 ('ROLE_MODERATOR'),
 ('ROLE_USER');
 
+
+INSERT INTO `account` (name, password, email, photo,confirm_token, enabled, mute) VALUES
+("admin","$2a$10$VWCYTN8oYPrw7riyRsyVz.PC0mkhJ3NwS8jcwF3ds0onBF7qd9d2.","admin@gmail.com","https://www.logolynx.com/images/logolynx/23/23938578fb8d88c02bc59906d12230f3.png","",1,'2015-11-05 14:29:36');
+
+INSERT INTO `accounts_and_roles` (id_name, id_authority) VALUES
+(1,1);
+
 SET FOREIGN_KEY_CHECKS = 1;
 
 Drop table if exists `files`;
@@ -55,13 +63,30 @@ CREATE TABLE `files`(
 primary key(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `user_file_likes`;
+CREATE TABLE `user_file_likes`(
+`id` int(16) NOT NULL AUTO_INCREMENT,
+`id_user` int(16) DEFAULT NULL,
+`id_file` int(16) DEFAULT NULL,
+
+PRIMARY KEY(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `user_file_dislikes`;
+CREATE TABLE `user_file_dislikes`(
+`id` int(16) NOT NULL AUTO_INCREMENT,
+`id_user` int(16) DEFAULT NULL,
+`id_file` int(16) DEFAULT NULL,
+
+PRIMARY KEY(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments`(
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `file_id` int(11) default null,
 `user_name`  varchar(256) default null,
-`comment` varchar(512) default null,
+`comment` varchar(10000) default null,
 `date` datetime DEFAULT NULL,
 
 primary key(`id`)
@@ -83,4 +108,13 @@ CREATE TABLE `reported_comments`(
 `reporting_user` varchar(256) default null,
 
 primary key(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `cubby`;
+CREATE TABLE `cubby`(
+`id` int(11) not null auto_increment,
+`email` varchar(256) DEFAULT NULL,
+`file_id` int(11) DEFAULT NULL,
+
+PRIMARY KEY(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
